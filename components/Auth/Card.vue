cce4abd2be6ef640fe244e57e58447481ee7c31d
Koichi.Shimomura <unieye51@gmail.com>
2022-07-28 09:00:14 +0900

    10-63 Building the Authentication Card

---
+++ components/Auth/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NCard class="card">
+      <h3>Login</h3>
+      <div class="input-container">
+        <input placeholder="Email" />
+        <input placeholder="Password" />
+      </div>
+      <NButton>Login</NButton>
+    </NCard>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 1rem;
+  width: 25rem;
+}
+
+.card h3 {
+  font-size: 1.5rem;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-container input {
+  margin-bottom: 0.3rem;
+  padding: 0.2rem;
+  outline: none;
+
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  border-radius: 0.2rem;
+}
+</style>
